da506ed018594789f979b2bf4e4de816e4383026
Jason Dewese <19419757+jasondewese@users.noreply.github.com>
2021-01-05 22:48:29 -0600

    Created all divs for page and made simple UI. Began implementing code with buttons. Error with player buttons-trigger multiple selections.

--- script.js
+++ script.js
@@ -3,8 +3,40 @@
 //https://mooniidev.github.io/rock-paper-scissors-game/
 
 
+
+/*************** 
+ * Author: Jason Dewese
+ * Initial Creation: 1/3/2021
+ * Last Edit: 1/5/2021
+ * 
+ * CURRENT ERROR: Rock button is triggering rock, paper,
+ * and scissors in the console and not displaying correct result.
+*/
+
+
 //begin the 5 round rock-paper-scissors game!
-game();
+//game();
+const gameResult = document.querySelector('#gameResults');
+
+
+//play game with 'Rock' as player selection
+const rockBtn = document.querySelector('#playerRock');
+rockBtn.addEventListener('click', () => {
+  playGame('Rock', computerPlay());  
+});
+
+//player game with 'Paper' as player selection
+const paperBtn = document.querySelector('#playerPaper');
+rockBtn.addEventListener('click', () => {
+  playGame('Paper', computerPlay());  
+});
+
+//player game with 'Scissors' as player selection
+const scissorsBtn = document.querySelector('#playerScissors');
+rockBtn.addEventListener('click', () => {
+  playGame('Scissors', computerPlay());  
+});
+
 
 
 
@@ -56,16 +88,19 @@ function playGame(playerSelection, computerSelection) {
     {
         if (computerSelection.toLowerCase() === 'rock')
         {
+            gameResult.textContent = "Draw! Rock ties Rock";
             console.log('Draw! Rock ties Rock');
             return 0;
         }
         if (computerSelection.toLowerCase() === 'paper')
         {
+            gameResult.textContent ='You Lose! Paper beats Rock';
             console.log('You Lose! Paper beats Rock');
             return -1;
         }
         if (computerSelection.toLowerCase() === 'scissors')
         {
+            gameResult.textContent = 'You Win! Rock beats Scissors';
             console.log('You Win! Rock beats Scissors');
             return 1;
         }
@@ -77,16 +112,19 @@ function playGame(playerSelection, computerSelection) {
     {
         if (computerSelection.toLowerCase() === 'rock')
         {
+            gameResult.textContent = 'You Win! Paper beats Rock';
             console.log('You Win! Paper beats Rock');
             return 1;
         }
         if (computerSelection.toLowerCase() === 'paper')
         {
+            gameResult.textContent = 'Draw! Paper ties Paper';
             console.log('Draw! Paper ties Paper');
             return 0;
         }
         if (computerSelection.toLowerCase() === 'scissors')
         {
+            gameResult.textContent = 'You Lose! Scissors beats Paper';
             console.log('You Lose! Scissors beats Paper');
             return -1;
         }
